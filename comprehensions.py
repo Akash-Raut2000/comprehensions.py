c519e2567995e08939e4319bade64f2284ddf70f
Akash Raut <136334052+Akash-Raut2000@users.noreply.github.com>
2024-12-20 22:11:31 +0530

    Add files via upload

---
+++ comprehensions.py
@@ -0,0 +1,14 @@
+#List comprehensions
+
+squares = [x**2 for x in range(5)]
+print(squares)
+
+# Dictionaries comprehensions
+
+Dict = { x:x**2 for x in range (5)}
+print (Dict)
+
+# Set Comprehensions
+
+Set = {x for x in [1,2,2,4]}
+print(Set)
